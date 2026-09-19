1af5abe38e575982ef03d559d2fece5b8f19dc88
volodimirrt <motchukvolodimir@gmail.com>
2026-09-19 11:58:09 +0300

    Refactor script.js to improve formatting and structure

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-
 function showText() {
     alert("Ласкаво просимо до JYSK!");
 }
 
+
 function buyProduct(productName) {
 
+
     alert(`Дякуємо за замовлення! Товар "${productName}" успішно додано до кошика.`);
-    
-  
+   
+ 
     console.log(`Користувач купив: ${productName}`);
 }
 
+
 document.addEventListener("DOMContentLoaded", function () {
     const navLinks = document.querySelectorAll("nav a");
+
 
     navLinks.forEach(link => {
         link.addEventListener("click", function (e) {
@@ -20,9 +23,9 @@ document.addEventListener("DOMContentLoaded", function () {
             if (targetId.startsWith("#")) {
                 e.preventDefault();
                 const targetElement = document.querySelector(targetId);
-                
+               
                 if (targetElement) {
-                    
+                   
                     targetElement.scrollIntoView({
                         behavior: "smooth"
                     });
@@ -31,3 +34,6 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+
